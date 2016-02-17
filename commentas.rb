38eb4e8279f4a7b2266318e9ad2434a957f82091
josias2121 <josias.vidrio.simontech.2016@gmail.com>
2016-02-17 22:07:27 +0000

    working

---
+++ commentas.rb
@@ -0,0 +1,5 @@
+# First, we need to place our logo in the top Left side of the page and on the right side place " created by -------Josias Vidrio and Ivan Saldana. 
+# Second, copy and paste code for <nav> bar from git bootstrap.com
+# Next place <div> between Home, Suggest and category. 
+# make a input form. 
+# Make page scrollable, only one page. 
